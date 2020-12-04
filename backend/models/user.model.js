955aa109d72a8ccc190be777eda4d1047e75e73a
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-12-04 15:41:31 +0530

    Adding Models for DB

---
+++ backend/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const  Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username : {
+        type: String,  //validations
+        required: true,
+        unique:true,
+        trim: true,
+        minlength: 3
+    },    
+},{
+    timestamps: true,  //create fields for when it was created and modified
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
