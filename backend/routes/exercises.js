7ce4f105a6b8d78ad75a8f32934d8ee5a9b62b6f
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-12-06 09:39:48 +0530

    Added CRUD Operations and created API end-points

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,12 +3,16 @@ let Exercise = require('../models/exercise.model');
 
 // read comments in user.js
 
+// all the crud operation APIs
+
+// READ
 router.route('/').get((req, res) => {
     Exercise.find()
         .then( exercises => res.json(exercises))
         .catch( err => res.status(400).json('Error: ' + err))
 });
 
+// CREATE
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -25,6 +29,38 @@ router.route('/add').post((req, res) => {
     newExercise.save()
         .then(() => res.json('Exercise Added!'))
         .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// get exercise object by id 
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.json("Error: " + err));
+});
+
+// DELETE
+router.route('/:id').post((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// UPDATE
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then( exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+            
+
+            //saving the updated exercise
+            exercise.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
