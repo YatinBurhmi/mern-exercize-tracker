f186819ecddc88583b1f12da5574c67de8c8fdbb
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-12-06 13:28:15 +0530

    Created Create-User UI

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,13 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CreateUsers extends Component {
-    render() {
-        return (
-            <div>
-                <p>You are on Create Users component!</p>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    //whenever state is updated, DOM is rendered
+    this.state = {
+      username: "",
+    };
+  }
+
+  // set username from text-box
+  onChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value, // updates
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    console.log(user);
+
+    this.setState({
+        username:'',
+    })
+
+    // window.location = "/";
+  };
+
+
+  render() {
+    return (
+      <div>
+       <h3>Create New User</h3>
+       <form onSubmit={this.onSubmit}>
+           <div className="form-group">
+               <label>Username: </label>
+               <input type="text"
+                    required
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+               />
+           </div>
+           <div className="form-group">
+               <input type="submit" value="Create User" className="btn btn-primary" />
+           </div>
+       </form>
+      </div>
+    );
+  }
 }
 
 export default CreateUsers;
